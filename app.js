bf6791390ad0f1d0a9dbc20765ed4248cec3e1e6
@chibaodang2002 <chibaodang2002@gmail.com>
2024-11-05 19:18:03 +0700

    EC2

---
+++ app.js
@@ -0,0 +1,47 @@
+import express from "express";
+import { createReadStream, statSync } from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.get("/video", (req, res) => {
+    const filepath = `${__dirname}/public/lu.mp4`;
+    const stat = statSync(filepath);
+    const fileSize = stat.size;
+
+    const range = req.headers.range;
+
+    if (!range) {
+        return res.status(400).send("Requires Range header");
+    }
+
+    const chunkSize = 10 ** 6;
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + chunkSize, fileSize - 1);
+
+    const contentLength = end - start + 1;
+
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4",
+    };
+
+    res.writeHead(206, headers);
+
+    const fileStream = createReadStream(filepath, { start, end });
+    fileStream.pipe(res);
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
